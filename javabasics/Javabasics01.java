e6409d82da5258e5613c216dde8307d4eb2c6684
Gobinat-Codes <140052579+Gobinat-Codes@users.noreply.github.com>
2024-04-12 23:46:45 +0530

    Add files via upload

---
+++ javabasics/Javabasics01.java
@@ -0,0 +1,10 @@
+package javabasics;
+
+public class Javabasics01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ javabasics/Javabasics02.java
@@ -0,0 +1,13 @@
+package javabasics;
+
+public class Javabasics02 {
+		public void hello() {
+			System.out.println("vanakam,namaskar");
+		}
+	public static void main(String[] args) {
+		Javabasics02 obj1 = new Javabasics02();
+		obj1.hello();
+
+	}
+
+}
